56003a6779ec91718d7d49656a7b68b0f659c944
Jonathan Magacho <53492271+magach07@users.noreply.github.com>
2023-03-03 13:04:48 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace EnviarEmail;
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            MailMessage mailMessage = new MailMessage("jonathanmagacho@gmail.com", "jonathanmagacho@gmail.com");
+
+            mailMessage.Subject = "Titulo de Exemplo";
+            mailMessage.IsBodyHtml = true;
+            mailMessage.Body = "<p> Esse é um e-mail enviado automaticamente pela API de Jonathan Magacho</p>";
+            mailMessage.SubjectEncoding = Encoding.GetEncoding("UTF-8");
+            mailMessage.BodyEncoding = Encoding.GetEncoding("UTF-8");
+
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential("jonathanmagacho@gmail.com", "bwnmlugilverfsbg");
+
+            client.EnableSsl = true;
+            
+            client.Send(mailMessage);
+
+            Console.Write("E-mail enviado com sucesso");
+        }
+        catch (Exception e)
+        {
+            Console.Write("Ocorreu um erro durante o envio do e-mail. " + e.Message);
+        }
+    }
+}
